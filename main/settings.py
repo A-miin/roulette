13aa07953d38cded4c594f57b74ff0a53e77d5e2
A-miin <zulaykaisaeva@gmail.com>
2022-07-07 09:35:52 +0300

    Dockerized the project

--- main/settings.py
+++ main/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'roulette',
 
     # lib apps
+    'rest_framework_simplejwt',
 
 ]
 
@@ -103,6 +104,11 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
